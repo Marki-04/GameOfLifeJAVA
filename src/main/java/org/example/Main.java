c9885d93dda524a16a69a7d3242adae9a6db32dd
fff@fff.fff <fff@fff.fff>
2025-02-03 09:27:24 -0500

    Afficher le jeu à l'infini en attendant une touche

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,34 +3,50 @@ package org.example;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Scanner;
 import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
+        // Créer le tableau
         char[][] tableau = new char[12][12];
 
-        // Création du tableau
+        // Ajouter des cellules selectionnées
+        tableau[3][3] = 'X';
+        tableau[3][4] = 'X';
+        tableau[4][4] = 'X';
+
+        // Créer un scanner pour la touche ENTER
+        Scanner scanner = new Scanner(System.in);
+        do{
+            // Afficher le tablea de jeu
+            afficherJeu(tableau);
+
+            // Attendre pour une touche
+            scanner.nextLine();
+
+            // Calculer la génération suivante
+
+
+        } while (true);
+
+
+    }
+    // Création du tableau
+    private static void afficherJeu(char[][] tableau) {
         for (int i = 1; i < 10; i++) {
             System.out.println(" ");
             for (int j = 1; j < 10; j++) {
                 // Ajout de cellules selectionnées :
-                if(i == 3 && j == 3) {tableau[i][j] = 'X';}
-                else if(i == 5 && j == 4) {tableau[i][j] = 'X';}
-                else if(i == 7 && j == 5) {tableau[i][j] = 'X';}
-                else if(i == 8 && j == 6) {tableau[i][j] = 'X';}
-                else if(i == 9 && j == 7) {tableau[i][j] = 'X';}
-                else if(i == 8 && j == 8) {tableau[i][j] = 'X';}
+                if (tableau[i][j] == 'X') {
+                    tableau[i][j] = 'X';
+                }
                 // Reste des cellules vides :
-                else {tableau[i][j] = ' ';}
+                else {
+                    tableau[i][j] = ' ';
+                }
                 System.out.printf("[" + String.valueOf(tableau[i][j]) + "]");
             }
         }
-
-        // LES CONDITIONS :
-
-
-
     }
-
-
 }
